ddc1107fc30f60642ec82259488d6114b7e9bd83
Victor Nieto <victornc83@gmail.com>
2016-01-22 05:31:45 +0100

    Errores menores

--- roles/graphite/files/scripts/graphite-purge.sh
+++ roles/graphite/files/scripts/graphite-purge.sh
@@ -3,5 +3,5 @@
 metricas=`carbon-list | carbon-sieve -I -n $1`
 
 for i in $metricas ; do
-  echo "/var/lib/carbon/whisper/$i" | tr '.' '/' | xargs -l{} rm {}.wsp ;
+  echo "/var/lib/carbon/whisper/$i" | tr '.' '/' | xargs -I{} rm {}.wsp ;
 done
--- roles/graphite/files/scripts/graphite-rebalance.sh
+++ roles/graphite/files/scripts/graphite-rebalance.sh
@@ -7,7 +7,5 @@ LOCAL_IP="$1"
 PATH_OLD_CLUSTER="$2"
 
 for h in $(carbon-hosts -c "$PATH_OLD_CLUSTER") ; do
-  ssh $h -- carbon-list |
-  carbon-sieve -n $LOCAL_IP |
-  carbon-sync -s $h
+  ssh $h -- carbon-list | carbon-sieve -n $LOCAL_IP | carbon-sync -s $h
 done
