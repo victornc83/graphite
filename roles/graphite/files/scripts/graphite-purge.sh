d59c9be2fbd6555b52f93ac42acd5b594e705091
Victor Nieto <victornc83@gmail.com>
2016-01-22 13:27:00 +0100

    Versión Final de graphite-purge.sh con salida.

--- roles/graphite/files/scripts/graphite-purge.sh
+++ roles/graphite/files/scripts/graphite-purge.sh
@@ -2,6 +2,9 @@
 
 metricas=`carbon-list | carbon-sieve -I -n $1`
 
+echo '$(date +"%D-%T") - Purgando metricas'
+
 for i in $metricas ; do
+  echo '$(date +"%D-%T") - $i'
   echo "/var/lib/carbon/whisper/$i" | tr '.' '/' | xargs -I{} rm {}.wsp ;
 done
